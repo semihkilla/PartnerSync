02cc1808a50724c24bc3938e2346b4941130e805
SemihErden <167226497+semihkilla@users.noreply.github.com>
2025-06-23 23:41:44 +0200

    fix: keep layout server-side

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -1,4 +1,6 @@
-"use client";
+import dynamic from "next/dynamic";
+
+const Header = dynamic(() => import("../components/Header"));
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
